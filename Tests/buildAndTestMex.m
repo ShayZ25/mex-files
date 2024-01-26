d8d2e36adb71805f59fff1d7d339836bdf679de2
Shay Zeevi <shay.zeevi@vayyar.com>
2024-01-26 13:21:10 +0200

    Added test files

---
+++ Tests/buildAndTestMex.m
@@ -0,0 +1,34 @@
+clear
+
+%% Configuration
+cFileName = 'arrayProduct.c';
+mexExtension = 'mexw64';
+apiVersion = 'R2018a';
+
+scalarTest = randn(1);
+arrayTest = randn(1, 10000);
+
+%% Build MEX
+
+[~,fileNameWithoutExtension] = fileparts(cFileName);
+mexFileName = [fileNameWithoutExtension, '.', mexExtension];
+
+if ~isfile(mexFileName) 
+    mex(cFileName, ['-', apiVersion]);
+end
+
+%% Test MEX
+
+mexResult = arrayProduct(scalarTest, arrayTest);
+matlabResult = scalarTest * arrayTest;
+
+numericError = max(abs(mexResult - matlabResult));
+assert(numericError == 0, 'There is a numeric error!');
+
+%% Test Runtime
+
+mexFunctionHandle = @() arrayProduct(scalarTest, arrayTest);
+fprintf('MEX runtime is %.3f micro sec.\n', timeit(mexFunctionHandle)*1e6);
+
+matlabFunctionHandle = @() scalarTest*arrayTest;
+fprintf('MATLAB runtime is %.3f micro sec.\n', timeit(matlabFunctionHandle)*1e6);
